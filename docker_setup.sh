b319105bea4c988c39e50f1729edad83ba6aae5b
Salvatore Spina <90448546+SalvatoreSpina@users.noreply.github.com>
2024-09-15 11:55:56 +0200

    No need for docker + redirect cache

--- docker_setup.sh
+++
@@ -1,6 +0,0 @@
-apt-get update && apt-get install -y \
-    libgl1-mesa-glx \
-    libglib2.0-0 \
-    && rm -rf /var/lib/apt/lists/*
-
-pip install --no-cache-dir -r requirements.txt
--- setup.sh
+++ setup.sh
@@ -3,22 +3,24 @@
 # Step 1: Create the local library directory for Python packages
 mkdir -p ./local_lib/python3.8/site-packages
 
-# Step 2: Export the PYTHONPATH
+# Step 2: Export the PYTHONPATH and set the PYTHONPYCACHEPREFIX to the local library
 export PYTHONPATH=$(pwd)/local_lib/python3.8/site-packages:$PYTHONPATH
+export PYTHONPYCACHEPREFIX=$(pwd)/local_lib/__pycache__
 
 # Step 3: Install required Python packages to the local library directory
 pip install --target=$(pwd)/local_lib/python3.8/site-packages numpy \
     torch torchvision pandas matplotlib pillow \
     flake8 mizani==0.9.2 plantcv==3.14.3 \
-    opencv-python-headless \
-    tensorflow
+    opencv-python-headless tensorflow
 
-# Step 4: Add the local library directory to the PYTHONPATH permanently
+# Step 4: Add the local library directory to the PYTHONPATH and PYTHONPYCACHEPREFIX permanently
 echo 'export PYTHONPATH=$(pwd)/local_lib/python3.8/site-packages:$PYTHONPATH' >> ~/.zshrc
+echo 'export PYTHONPYCACHEPREFIX=$(pwd)/local_lib/__pycache__' >> ~/.zshrc
 
 # Step 5: Reload the shell configuration
 source ~/.zshrc
 
+# Alias for flake8
 alias norminette_python=flake8
 
-echo "Setup complete. The environment has been configured to use locally installed Python packages."
+echo "Setup complete. The environment has been configured to use locally installed Python packages and cache redirected."
