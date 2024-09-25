cde9c1ec8d8b8f0f1326f9e88ab613136601e748
Salvatore SPINA <sspina@ncelrnd2747.nce.amadeus.net>
2024-09-25 11:34:53 +0200

    Normed Cam

--- Part_4/cam.py
+++ Part_4/cam.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import argparse
 
+
 def capture_photo(fruit):
     # Open the webcam
     cam = cv2.VideoCapture(0)
@@ -17,20 +18,20 @@ def capture_photo(fruit):
         if not ret:
             print("Error: Failed to capture image.")
             break
-        
+
         # Get frame dimensions
         height, width = frame.shape[:2]
-        
+
         # Define square dimensions (80% of the smaller frame dimension)
         side_length = int(min(width, height) * 0.8)
         x1 = int((width - side_length) / 2)
         y1 = int((height - side_length) / 2)
         x2 = x1 + side_length
         y2 = y1 + side_length
-        
+
         # Draw square on the frame
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        
+
         # Display the frame with the square
         cv2.imshow("Press Space to Capture", frame)
 
@@ -46,7 +47,7 @@ def capture_photo(fruit):
             cropped_frame = frame[y1:y2, x1:x2]
             # Resize the cropped image to 150x150
             resized_frame = cv2.resize(cropped_frame, (150, 150))
-            
+
             img_name = f"{fruit}/cam.png"
             os.makedirs(os.path.dirname(img_name), exist_ok=True)
             cv2.imwrite(img_name, resized_frame)
@@ -57,14 +58,18 @@ def capture_photo(fruit):
     cam.release()
     cv2.destroyAllWindows()
 
+
 def run_predict(fruit):
-    command = ["python3", "predict.py", f"splitted/datasets/{fruit}/training/{fruit}", f"{fruit}/cam.png"]
+    arg_1 = f"splitted/datasets/{fruit}/training/{fruit}"
+    command = ["python3", "predict.py", arg_1, f"{fruit}/cam.png"]
     try:
         print(f'Execute command: {" ".join(command)}')
-        #result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        #print("Prediction Output:\n", result.stdout.decode('utf-8'))
+        # result = subprocess.run(command, check=True, \
+        #   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # print("Prediction Output:\n", result.stdout.decode('utf-8'))
     except subprocess.CalledProcessError as e:
         print("Error during prediction execution:\n", e.stderr.decode('utf-8'))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
